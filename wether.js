f5d79e1c6e1fd67630ac96f5b2a333437528c54e
tushar-1910 <102805884+tushar-1910@users.noreply.github.com>
2022-05-02 22:33:57 +0530

    unit5 c2

--- wether.js
+++ wether.js
@@ -1,28 +1,28 @@
 var url;
 async function getdata() {
-    let city = document.getElementById('city').value;
-    url = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=502df7b997e27e1e511693282e40f2f4';
-    let response = await fetch(url);
-    console.log(response);
+    try {
 
-    let result = await response.json();
-    console.log(result)
-    if(result == "")
-    {
-        alert("Please enter correct city name");
-        window.location.reload();
-    }
-    else
-    {
+        let city = document.getElementById('city').value;
+        url = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=502df7b997e27e1e511693282e40f2f4';
+        let response = await fetch(url);
+        console.log(response);
+
+        let result = await response.json();
+        console.log(result)
+
         let lati = result[0].lat;
         let long = result[0].lon
         console.log(lati, long)
-    
+
         url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lati + '&lon=' + long + '&units=metric&appid=502df7b997e27e1e511693282e40f2f4';
         response = await fetch(url);
         let report = await response.json();
         console.log(report);
         display(report);
+
+    }
+    catch (error) {
+        alert("Please Enter correct city name");
     }
 
 
@@ -36,45 +36,43 @@ function display(report) {
     head.innerText = "Current weather";
 
     var div1 = document.createElement('div');
-    div1.setAttribute('class','div1');
+    div1.setAttribute('class', 'div1');
     var img = document.createElement('img')
-    if(report.weather[0].description == "clear sky")
-    {
+    if (report.weather[0].description == "clear sky") {
         img.src = "https://cdn-icons-png.flaticon.com/512/869/869869.png";
     }
-    else
-    {
+    else {
         img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaGMABdWmoWMoV_ny-keV8Bc0BJcgJKD1a9g&usqp=CAU"
     }
 
     var temp = document.createElement("h3");
-    temp.innerText ="Temp : "+ report.main.temp +" °C";
-    
-    var description = document.createElement("p");
-    description.innerText ="Weather description : "+ report.weather[0].description;
+    temp.innerText = "Temp : " + report.main.temp + " °C";
 
-    div1.append(img,temp,description);
+    var description = document.createElement("p");
+    description.innerText = "Weather description : " + report.weather[0].description;
+
+    div1.append(img, temp, description);
 
     var div2 = document.createElement('div');
-    div2.setAttribute('class','div2');
+    div2.setAttribute('class', 'div2');
 
 
     var pressure = document.createElement("p");
-    pressure.innerText ="Pressure : "+ report.main.pressure +" hPa";
+    pressure.innerText = "Pressure : " + report.main.pressure + " hPa";
 
     var humidity = document.createElement("p");
-    humidity.innerText ="Humidity : "+ report.main.humidity +" %";
-    
+    humidity.innerText = "Humidity : " + report.main.humidity + " %";
+
     var wind = document.createElement("p");
-    wind.innerText ="Wind speed : "+ report.wind.speed +" m/s";
+    wind.innerText = "Wind speed : " + report.wind.speed + " m/s";
 
     var sunrise = document.createElement("p");
-    sunrise.innerText ="Sunrise : "+ report.sys.sunrise +" UTC";
+    sunrise.innerText = "Sunrise : " + report.sys.sunrise + " UTC";
 
     var sunset = document.createElement("p");
-    sunset.innerText ="Sunset : "+ report.sys.sunset +" UTC";
+    sunset.innerText = "Sunset : " + report.sys.sunset + " UTC";
 
-    div2.append(pressure,humidity,wind,sunrise,sunset);
+    div2.append(pressure, humidity, wind, sunrise, sunset);
 
-    document.getElementById('weather').append(div1,div2);
+    document.getElementById('weather').append(div1, div2);
 }
